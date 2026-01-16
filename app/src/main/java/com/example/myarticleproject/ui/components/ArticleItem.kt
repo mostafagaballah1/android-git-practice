4c894d8e323b19e0fb865d7e07998ee7d3d52784
MoustafaSPH <moustafa@sph.com.sg>
2026-01-16 10:40:13 +0200

    feature/add-article-ui

---
+++ app/src/main/java/com/example/myarticleproject/ui/components/ArticleItem.kt
@@ -0,0 +1,43 @@
+package com.example.myarticleproject.ui.components
+
+import android.R
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.magnifier
+import androidx.compose.foundation.text.contextmenu.modifier.appendTextContextMenuComponents
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.myarticleproject.model.Article
+
+@Composable
+fun ArticleItem(article: Article) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Column(
+            Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = article.title,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Spacer(Modifier.padding(4.dp))
+            Text(
+                text = article.description,
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Spacer(Modifier.padding(8.dp))
+            Text( text = "Date {${article.date}}")
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/myarticleproject/ui/screens/ArticleScreen.kt
@@ -0,0 +1,4 @@
+package com.example.myarticleproject.ui.screens
+
+class ArticleScreen {
+}
